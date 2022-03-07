a964a9a3f3f161b16e496688f9bccca731f2becf
hoonjo1 <hoonjo1@gmail.com>
2022-03-07 21:53:17 +0900

    2022-03-07

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-function test(a, b){
-    console.log("+",a+b)
-    console.log("-",a-b)
-    console.log("/",a/b)
+const age = parseInt(prompt("How old are you?"));
+
+
+if(isNaN(age)){
+    console.log("Please write a number");
+} else if (age<18){
+console.log("test");
+}  else if (age >=18 && age <=50) {
+    console.log("You can drink");
+}
+else {
+    console.log("You can't drink");
 }
 
-
-
-test(10,2);
