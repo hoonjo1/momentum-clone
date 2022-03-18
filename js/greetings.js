721d604b5da4ec540d0db720cfb68a0bdf24eb28
hoonjo1 <hoonjo1@gmail.com>
2022-03-18 17:14:56 +0900

    text change

--- js/greetings.js
+++ js/greetings.js
@@ -20,7 +20,7 @@ function onLoginSubmit(event) {
 }
 
 function paintGreetings(username){
-    greeting.innerText = `Wellcom  ${username}`;
+    greeting.innerText = `Wellcome  ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME)
     clock.classList.remove(HIDDEN_CLASSNAME)
     mid.classList.remove(HIDDEN_CLASSNAME);
